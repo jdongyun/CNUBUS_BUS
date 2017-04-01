1b2e16a94ce0b704af793001b7e158c7f1f6a883
Dongyun <wfmtooopa@gmail.com>
2017-04-01 19:42:19 +0900

    Add Okhttp

--- src/main/java/cnu/busstop/BusSetActivity.java
+++ src/main/java/cnu/busstop/BusSetActivity.java
@@ -13,16 +13,20 @@ public class BusSetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        SharedPreferences pref = getSharedPreferences("busStop", MODE_PRIVATE);
+
+        if(!pref.getString("busStyle", "NULL").equals("NULL") && !getIntent().getBooleanExtra("changeBus", false)) {
+            //전자는 처음 시작일 때 데이터가 있는지 없는지 체크, 후자는 MainActivity에서 노선 변경 버튼을 눌렀을 때 Intent에서 전송되는 값
+            startActivity(new Intent(BusSetActivity.this, MainActivity.class));
+        }
+
         setContentView(R.layout.activity_setbus);
 
-        SharedPreferences pref = getSharedPreferences("busStop", MODE_PRIVATE);
         final SharedPreferences.Editor editor = pref.edit();
-
-        pref.getString("busStyle", "NULL");
-
 
         Button buttonBusA = (Button) findViewById(R.id.button_bus_a);
         Button buttonBusB = (Button) findViewById(R.id.button_bus_b);
+        Button buttonBusPause = (Button) findViewById(R.id.button_bus_pause);
 
         buttonBusA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +45,21 @@ public class BusSetActivity extends AppCompatActivity {
                 startActivity(new Intent(BusSetActivity.this, MainActivity.class));
             }
         });
+        buttonBusPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("busStyle", "X")
+                        .apply();
+                startActivity(new Intent(BusSetActivity.this, MainActivity.class));
+            }
+        });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        //뒤로가기를 방지한다.
+        return;
     }
 }
--- src/main/java/cnu/busstop/MainActivity.java
+++ src/main/java/cnu/busstop/MainActivity.java
@@ -15,50 +15,163 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     int GPS_SYNC_TIME = 1000;
     int GPS_SYNC_METER = 1;
+    boolean THREAD_LOOP= true;
+
+    Double latitude, longitude;
+    float accuracy;
+    Double tmp_latitude = 0.0, tmp_longitude = 0.0;
+
     String LOG_TAG = "CNUBUS_forBus";
 
-    private TextView tvLatitude;
-    private TextView tvLongitude;
-    private TextView tvAccuracy;
-    private EditText etLatLong;
+    TextView tvBusStyle;
+    TextView tvLatitude;
+    TextView tvLongitude;
+    TextView tvGPSProvider;
 
     LocationManager mLM;
+
+    OkHttpClient client;
+    Thread connThread;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        tvLatitude = (TextView) findViewById(R.id.tv_latitude);
-        tvLongitude = (TextView) findViewById(R.id.tv_longitude);
-        tvAccuracy = (TextView) findViewById(R.id.tv_accuracy);
-        etLatLong = (EditText) findViewById(R.id.et_latlong);
+
 
         SharedPreferences pref = getSharedPreferences("busStop", MODE_PRIVATE);
         if(pref.getString("busStyle", "NULL").equals("NULL")) { //처음 실행시 버스종류 지정이 안됐을 때
             startActivity(new Intent(MainActivity.this, BusSetActivity.class));
 
         } else { //버스 종류가 지정되어 있을 때
-
             setContentView(R.layout.activity_main);
+
+            tvBusStyle = (TextView) findViewById(R.id.tv_busstyle);
+            tvLatitude = (TextView) findViewById(R.id.tv_latitude);
+            tvLongitude = (TextView) findViewById(R.id.tv_longitude);
+            tvGPSProvider = (TextView) findViewById(R.id.tv_gps_provider);
             if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 checkLocationPermission();
             }
 
 
-            mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Button btnChangeBus = (Button) findViewById(R.id.btn_changebus);
+            btnChangeBus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, BusSetActivity.class);
+                    intent.putExtra("changeBus", true);
+                    startActivity(intent);
+                }
+            });
 
-            mLM.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    GPS_SYNC_TIME, GPS_SYNC_METER, mLocationListener);
-            mLM.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    GPS_SYNC_TIME, GPS_SYNC_METER, mLocationListener);
+
+            if(pref.getString("busStyle", "NULL").equals("X")) { //버스가 운행중이 아닐 때
+                tvBusStyle.setText("중지");
+
+
+            } else { //버스가 운행중일 때
+                tvBusStyle.setText(pref.getString("busStyle", ""));
+
+                mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                mLM.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        GPS_SYNC_TIME, GPS_SYNC_METER, mLocationListener);
+                mLM.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        GPS_SYNC_TIME, GPS_SYNC_METER, mLocationListener);
+
+                client = new OkHttpClient();
+
+                connThread = new Thread() {
+                    @Override
+                    public void run() {
+
+                        while(THREAD_LOOP) {
+                            try {
+                                if(latitude != null &&
+                                        tmp_latitude != latitude &&
+                                        tmp_longitude != longitude) {
+
+                                    RequestBody formBody = new FormBody.Builder()
+                                            .add("id", "q555")
+                                            .add("lat", String.valueOf(latitude))
+                                            .add("lng", String.valueOf(longitude))
+                                            .add("accu", String.valueOf(accuracy))
+                                            .add("route", pref.getString("busStyle", "NULL"))
+                                            .add("time", String.valueOf(System.currentTimeMillis()/1000L))
+                                            .build();
+
+                                    Request request = new Request.Builder()
+                                            .url("https://cnubus.dyun.kr/location/")
+                                            .post(formBody)
+                                            .build();
+                                    tmp_latitude = latitude;
+                                    tmp_longitude = longitude;
+
+                                    client.newCall(request).enqueue(new Callback() {
+                                        @Override
+                                        public void onFailure(Call call, IOException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        @Override
+                                        public void onResponse(Call call, Response response) throws IOException {
+                                            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                                            Headers responseHeaders = response.headers();
+                                            for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                                                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                                            }
+
+                                            System.out.println(response.body().string());
+                                        }
+                                    });
+                                }
+
+                                Thread.sleep(3000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+
+
+
+                        }
+
+
+                    }
+                };
+                connThread.start();
+
+            }
+
+
+
+
+
 
 /*
             final Handler handler = new Handler();
@@ -150,15 +263,25 @@ public class MainActivity extends AppCompatActivity {
 
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
-            double longitude = location.getLongitude();    //경도
-            double latitude = location.getLatitude();         //위도
-            float accuracy = location.getAccuracy();        //정확도
+            longitude = location.getLongitude();    //경도
+            latitude = location.getLatitude();         //위도
+            accuracy = location.getAccuracy();        //정확도
             Log.i(LOG_TAG, latitude + "," + longitude + " accuracy : " + accuracy);
+            if(longitude != null) {
 
-            tvLatitude.setText(Double.toString(latitude));
-            tvLongitude.setText(Double.toString(longitude));
-            tvAccuracy.setText(Float.toString(accuracy));
-            etLatLong.setText(latitude + "," + longitude);
+
+                tvLatitude.setText(Double.toString(latitude));
+                tvLongitude.setText(Double.toString(longitude));
+                //tvAccuracy.setText(Float.toString(accuracy));
+
+                if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+                    tvGPSProvider.setText("GPS에 연결되었습니다.\n정확도 : " + accuracy);
+                } else {
+                    tvGPSProvider.setText("아직 GPS에 연결되지 않았습니다.");
+                }
+            }
+
+
         }
 
         public void onProviderDisabled(String provider) {
@@ -170,4 +293,22 @@ public class MainActivity extends AppCompatActivity {
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        //뒤로가기를 방지한다.
+        return;
+    }
+
+    @Override
+    protected void onStop() {
+        //노선 변경으로 Activity가 변경되었을 때 LocationManager의 Update를 해제한다.
+        super.onStop();
+        if(mLM != null) //운행이 중지되었을 때에는 LocationManager이 할당되지 않았으므로 NPE가 발생함.
+            mLM.removeUpdates(mLocationListener);
+        if(connThread != null) {
+            connThread.interrupt();
+            THREAD_LOOP = false; //Thread에서 더 이상 LOOP를 돌리지 않음
+        }
+    }
 }
